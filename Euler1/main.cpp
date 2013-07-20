298cc22b27b53e074b0734d1a79c9f639597d548
Kevin <klee946@gmail.com>
2013-07-19 21:18:22 -0500

    first commit

---
+++ Euler1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Find the sum of all the multiples 
+// of 3 or 5 below 1000.
+int main()
+{
+  int sum = 0;
+  for(int i = 0; i < 1000; i++)
+  {
+    if(i % 3 == 0 || i % 5 == 0)
+      sum += i;
+  }
+  std::cout << sum << std::endl;
+  return 0;
+}
